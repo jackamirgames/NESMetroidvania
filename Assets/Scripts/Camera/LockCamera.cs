abb8b378054df0d4d0342678634c61784f22144b
jackamirebrahimi <jackamirebrahimi@gmail.com>
2026-02-22 15:09:13 +0000

    Made the camera follow the player
    Made the camera follow the player character.

---
+++ Assets/Scripts/Camera/LockCamera.cs
@@ -0,0 +1,21 @@
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class LockCamera : CinemachineExtension
+{
+    //We wanna lock either the x or y axis of the camera
+
+    //Grab Cinemachine from gameobject
+    //Get the tracking target transform and cache as variable
+
+    //
+
+    [Tooltip("Lock the camera's X position to this value")]
+
+
+
+    private void Awake()
+    {
+        
+    }
+}
