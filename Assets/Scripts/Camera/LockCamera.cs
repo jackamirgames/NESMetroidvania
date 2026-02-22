f5d06a49cd29d3c4a1763cac9636ebd9a5348a1c
jackamirebrahimi <jackamirebrahimi@gmail.com>
2026-02-22 16:31:17 +0000

    Added a cinemachine extension
    Added a cinemachine extension which locks either the x or y position of the camera. Each can be toggled on/off for 4 combinations.

--- Assets/Scripts/Camera/LockCamera.cs
+++ Assets/Scripts/Camera/LockCamera.cs
@@ -3,19 +3,45 @@ using UnityEngine;
 
 public class LockCamera : CinemachineExtension
 {
-    //We wanna lock either the x or y axis of the camera
-
-    //Grab Cinemachine from gameobject
-    //Get the tracking target transform and cache as variable
-
-    //
-
+    [Header("X-Pos")]
+    public bool lockXPos;
     [Tooltip("Lock the camera's X position to this value")]
+    public float m_XPosition = 0;
 
+    [Header("Y-Pos")]
+    public bool lockYPos;
+    [Tooltip("Lock the camera's Y position to this value")]
+    public float m_YPosition = 0;
 
-
-    private void Awake()
+    protected override void PostPipelineStageCallback(CinemachineVirtualCameraBase vcam, CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
     {
-        
+        LockXAxis(vcam, stage, ref state, deltaTime);
+        LockYAxis(vcam, stage, ref state, deltaTime);
+    }
+
+    private void LockXAxis(CinemachineVirtualCameraBase vcam, CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
+    {
+        if (!lockXPos) return;
+
+        if (enabled && stage == CinemachineCore.Stage.Finalize)
+        {
+            var pos = state.GetCorrectedPosition();
+            pos.x = m_XPosition;
+            state.RawPosition = pos;
+            state.PositionCorrection = Vector3.zero;
+        }
+    }
+
+    private void LockYAxis(CinemachineVirtualCameraBase vcam, CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
+    {
+        if (!lockYPos) return;
+
+        if (enabled && stage == CinemachineCore.Stage.Finalize)
+        {
+            var pos = state.GetCorrectedPosition();
+            pos.y = m_YPosition;
+            state.RawPosition = pos;
+            state.PositionCorrection = Vector3.zero;
+        }
     }
 }
