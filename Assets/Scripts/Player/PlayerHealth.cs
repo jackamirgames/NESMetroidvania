35f8d5b6164ce49a95d59fe075094df319c7235b
jackamirebrahimi <jackamirebrahimi@gmail.com>
2026-03-03 20:16:43 +0000

    Added player health and started stalagtite
    Added the player health script. Also began work on a stalactite, which will fall and harm the player if it hits them.

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour, IDamageable
+{
+    private int _health;
+    public int Health
+    {
+        get { return _health; }
+        set { _health = value; }
+    }
+
+    [SerializeField] private int _maxHealth;
+    public int MaxHealth
+    {
+        get { return _maxHealth; }
+        set { _maxHealth = value; }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        Health -= damage;
+
+        if (Health <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+}
